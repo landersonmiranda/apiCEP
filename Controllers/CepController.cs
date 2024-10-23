388dce91ecf3c28615396895858cf888e0a369c7
LandersonMiranda <80685802+LandersonMiranda@users.noreply.github.com>
2024-10-23 15:18:44 -0300

    subindo projeto

---
+++ Controllers/CepController.cs
@@ -0,0 +1,54 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using ProjetoCep.Models;
+
+namespace ProjetoCep.Controllers
+{
+    public class CepController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> ConsultaCep(string cep)
+        {
+            if (string.IsNullOrWhiteSpace(cep))
+            {
+                return View("Index");
+            }
+
+            Endereco endereco = await ObterEnderecoAsync(cep);
+
+            if (endereco == null)
+            {
+                ViewBag.Error = "CEP não encontrado.";
+                return View("Index");
+            }
+
+            return View("Resultado", endereco);
+        }
+
+        private async Task<Endereco> ObterEnderecoAsync(string cep)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                string url = $"https://viacep.com.br/ws/{cep}/json/";
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    string jsonResponse = await response.Content.ReadAsStringAsync();
+                    Endereco endereco = JsonConvert.DeserializeObject<Endereco>(jsonResponse);
+                    return endereco;
+                }
+            }
+
+            return null;
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjetoCep.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
